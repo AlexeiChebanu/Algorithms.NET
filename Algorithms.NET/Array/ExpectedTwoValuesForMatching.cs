0f92f1b0236d385d75a157ce0a9f1fd128491056
Alexei Chebanu <0alexchebanu@gmail.com>
2023-05-29 14:21:18 +0300

    Add project files.

---
+++ Algorithms.NET/Array/ExpectedTwoValuesForMatching.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.NET.Array
+{
+    internal class ExpectedTwoValuesForMatching
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            var seen = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int complement = target - nums[i];
+                if (seen.ContainsKey(complement))
+                {
+                    return new int[] { seen[complement], i };
+                }
+                seen[nums[i]] = i;
+            }
+            return new int[0];
+        }
+    }
+}
---
+++ Algorithms.NET/String/IntegerToRoman.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.NET.String
+{
+    internal class IntegerToRoman
+    {
+        public string IntToRoman(int num)
+        {
+            var DivisionBy10 = new string[] { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+            var DivisionBy100 = new string[] { "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
+            var DivisionBy1000 = new string[] { "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" };
+            var DivisionBy10000 = new string[] { "", "M", "MM", "MMM" };
+
+            return DivisionBy10000[num / 1000] + DivisionBy1000[(num % 1000) / 100] + DivisionBy100[(num % 100) / 10] + DivisionBy10[num % 10];
+        }
+    }
+}
