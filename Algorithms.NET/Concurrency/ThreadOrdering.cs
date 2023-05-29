d872474bad5dfee3e13162a5f73268c7fc08f295
Alexei Chebanu <0alexchebanu@gmail.com>
2023-05-29 15:58:16 +0300

    Semaphore

--- Algorithms.NET/Concurrency/ThreadOrdering.cs
+++ Algorithms.NET/Concurrency/ThreadOrdering.cs
@@ -4,9 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Algorithms.NET.Concurrency
+namespace Algorithms.NET.Concurrency;
+
+internal class ThreadOrdering
 {
-    internal class ThreadOrdering
+
+    private static Semaphore _s2;
+    private static Semaphore _s3;
+
+    public ThreadOrdering()
     {
+        _s2 = new Semaphore(initialCount: 0, maximumCount: 1);
+        _s3 = new Semaphore(initialCount: 0, maximumCount: 1);
+    }
+
+    public void First(Action printFirst)
+    {
+
+        printFirst();
+        _s2.Release();
+    }
+
+    public void Second(Action printSecond)
+    {
+
+        _s2.WaitOne();
+        printSecond();
+        _s3.Release();
+    }
+
+    public void Third(Action printThird)
+    {
+        _s3.WaitOne();
+        printThird();
     }
 }
