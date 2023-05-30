bbb4471fbd210493586f56c9407f0d5e5710fdd7
Alexei Chebanu <0alexchebanu@gmail.com>
2023-05-31 01:42:18 +0300

    Power. Recursion way

---
+++ Algorithms.NET/Algorithms/Power.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.NET.Algorithms;
+
+internal class Power
+{
+    public static double MyPow(double x, int n)
+    {
+        if (n < 0) { x = 1 / x; }
+        return RecPow(x, n);
+    }
+
+    private static double RecPow(double x, int n)
+    {
+        if (n == 0) return 1;
+        if (n == 1 || n == -1) return x;
+        double res = RecPow(x, n / 2);
+        return res * res * (n % 2 == 0 ? 1 : x);
+    }
+}
--- Algorithms.NET/Program.cs
+++ Algorithms.NET/Program.cs
@@ -1,1 +1,3 @@
-﻿
+﻿using Algorithms.NET.Algorithms;
+
+Console.WriteLine(Power.MyPow(4,5));
