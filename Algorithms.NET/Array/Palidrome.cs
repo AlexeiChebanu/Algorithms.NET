63c01e62e6296e0131572e11371c2da143b9c4c1
Alexei Chebanu <0alexchebanu@gmail.com>
2023-06-01 22:25:43 +0300

    IsPalindrome

---
+++ Algorithms.NET/Array/Palidrome.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.NET.Array;
+
+internal class Palidrome
+{
+    public static bool IsPalindrome(int x)
+    {
+        var y = x.ToString().ToCharArray();
+        System.Array.Reverse(y);
+        return x.ToString() == new string(y);
+    }
+}
