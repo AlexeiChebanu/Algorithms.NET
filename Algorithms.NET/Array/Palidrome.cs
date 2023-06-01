b399d49ca271e3bef4a85e0fbdae394bea9a1fbe
Alexei Chebanu <0alexchebanu@gmail.com>
2023-06-01 22:34:27 +0300

    IsPalindrome_v_2

--- Algorithms.NET/Array/Palidrome.cs
+++ Algorithms.NET/Array/Palidrome.cs
@@ -14,4 +14,15 @@ internal class Palidrome
         System.Array.Reverse(y);
         return x.ToString() == new string(y);
     }
+
+    public static bool IsPalindrome2(int x)
+    {
+        if (x < 0) return false;
+        var x_string = x.ToString();
+        for (int i = 0; i < x_string.Length; i++)
+        {
+            if (x_string[i] != x_string[x_string.Length - 1 - i]) return false;
+        }
+        return true;
+    }
 }
